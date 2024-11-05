c4a2f661bc49e505147004a1999da011db31972d
Clara <clavalpe@upv.es>
2024-11-05 21:32:17 +0100

    green - codebreaker guesses all blue combination

--- src/mastermind.py
+++ src/mastermind.py
@@ -1,4 +1,4 @@
 class Codemaker:
     def __init__(self, code:list[str]) -> None: ...
     def evaluate(self, guess:list[str]) -> list[int]: 
-        raise NotImplementedError
+        return [4, 0]
